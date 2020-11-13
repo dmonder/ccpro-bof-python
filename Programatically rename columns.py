e97526222116aacaa155278b15bb9c1174c5dec1
aew5044 <55103904+aew5044@users.noreply.github.com>
2020-11-13 15:32:42 -0500

    Update Programatically rename columns.py

--- Programatically rename columns.py
+++ Programatically rename columns.py
@@ -14,3 +14,14 @@ print(stc.columns)
 # the sequence "Stc" possibly (the ?) followed by whitespace (\s) and delete them.
 stc.columns = stc.columns.str.replace(r"\s|Stc\s?","")
 print(stc.columns)
+
+#this line looks for more funkyness in the column names
+stc.columns = stc.columns.str.replace(r"\s|Stc\s?|X\sSec\s?|Scs\s?|X\sScs\s?","")
+#this addes a prefix to the column names
+stc = stc.add_prefix('scs_')
+
+#this looks for all columns with the word data and casts them to the object date_cols
+date_cols = [col for col in stc.columns if 'Date' in col]
+
+#this turns the columns into actual dates vs. txt formated values.
+stc[date_cols]=stc[date_cols].apply(pd.to_datetime)
